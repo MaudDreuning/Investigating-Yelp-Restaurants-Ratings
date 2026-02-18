1aca37468295ddb0ded1081a10a12d1cb9c4a08c
WillemvdMerbel <W.n.vdmerbel@tilburguniversity.edu>
2026-02-18 12:12:26 +0100

    Adding first data preperation, leaving only restaurants and creating variable price_range

--- src/data-preparation/data-cleaning.R
+++ src/data-preparation/data-cleaning.R
@@ -1,6 +1,25 @@
 # In this directory, you will keep all source code files relevant for 
 # preparing/cleaning your data.
 
-test123
+library(dplyr)
+library(stringr)
 
-terminal test
+dataset_restaurants <- Dataset %>%
+  filter(str_detect(categories, regex("\\bRestaurants\\b", ignore_case = TRUE)))
+view(dataset_restaurants)
+dataset_restaurants <- dataset_restaurants %>% filter(str_detect(attributes, "RestaurantsPriceRange2"))
+
+
+dataset_restaurants <- dataset_restaurants %>%
+  mutate(
+    price_range = as.numeric(
+      str_match(
+        attributes,
+        "RestaurantsPriceRange2'\\s*:\\s*'([1-5])"
+      )[,2]
+    )
+  )
+
+
+
+dataset_restaurants <- dataset_restaurants %>% filter(!is.na(price_range))
